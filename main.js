7d46278e14880f74b7c221d25318ff60115c98bf
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-01 19:23:33 +0300

    Variables Introduction

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-console.log(typeof "Hossam Shiref");
-console.log(typeof 500);
-console.log(typeof 600.6);
-console.log(typeof true);
-console.log(typeof false);
-console.log(typeof [10, 20, 30]);
-console.log(typeof ["hossam", "shiref", "ahmed"]);
-console.log(typeof { name: "Hossam", age: 15, state: true });
-console.log(typeof undefined);
-console.log(typeof null);
+// syntax => keyword variable_name = variable_value
+// you can make a variable with id in html that known global variable
+var name = "Hossam";
+var age = 15;
+var state = true;
+var students = ["hossam", "ahmed", "shiref"];
+var all = { name: "hoss", age: 14 };
+var not = undefined;
+var nul = null;
+console.log(name);
+console.log(age);
+console.log(state);
+console.log(students);
+console.log(all);
+console.log(not);
+console.log(nul);
+console.log(hello);
