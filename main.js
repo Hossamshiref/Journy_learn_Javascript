6b8f18c35501e34d7c205059a50373e9d2ab053c
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-02 23:38:52 +0300

    Concatenate Template Literals Template Strings

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-let name_one = "Hossam";
-let name_two = "Shiref";
-document.write(name_one + " " + name_two);
-console.log(name_one + " " + name_two);
-console.log(name_one, name_two);
+let a = "I";
+let b = "Love";
+let c = "you";
+console.log(a + " " + b + " " + c);
+console.log(`${a} ${b} ${c}`);
+let name = "Hossam";
+let welcome = "Hello";
+let markUp = `
+    <div class="card">
+        <div class="child>
+            <h2>${welcome}</h2>
+             <p>${name}</p>
+        </div>
+    </div>`;
+
+document.write(markUp);
