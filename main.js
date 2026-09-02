90b717226b1ec6191fec01ccf2a0a415a6534cf4
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-02 22:51:32 +0300

    String Syntax And Character Escape Sequences

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-console.log(x);
-console.log(b);
-// var x = 10;
-let x = 10;
-const b = 10;
-// var a = 10;
-// var a = 20;
-let c = 10;
-let c = 20;
-const d = 10;
-const d = 20;
-console.log(a);
-// the best is use let or const
+console.log('Hossam "shiref"');
+console.log("Hossam 'Shiref'");
+console.log('"Hossam Shiref"');
+console.log('Hossam "Shiref"');
+console.log("Hossam \\ Shiref");
+console.log(
+  "Hossam\
+ Shiref\
+ Ahmed",
+);
+console.log("Hossam\nShiref\nAhmed");
