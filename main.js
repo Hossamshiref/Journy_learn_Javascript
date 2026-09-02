22f5c265896ee34b5b58b6845f9f16137e0ad792
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-02 22:09:00 +0300

    Var, Let, Const

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-// syntax => keyword variable_name = variable_value
-// you can make a variable with id in html that known global variable
-var name = "Hossam";
-var age = 15;
-var state = true;
-var students = ["hossam", "ahmed", "shiref"];
-var all = { name: "hoss", age: 14 };
-var not = undefined;
-var nul = null;
-console.log(name);
-console.log(age);
-console.log(state);
-console.log(students);
-console.log(all);
-console.log(not);
-console.log(nul);
-console.log(hello);
+console.log(x);
+console.log(b);
+// var x = 10;
+let x = 10;
+const b = 10;
+// var a = 10;
+// var a = 20;
+let c = 10;
+let c = 20;
+const d = 10;
+const d = 20;
+console.log(a);
+// the best is use let or const
