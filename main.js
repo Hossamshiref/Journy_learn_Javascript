6b35379679cf4f5b3c2381be12b814d2409e16be
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-03 22:38:29 +0300

    Assignment_2

--- main.js
+++ main.js
@@ -1,15 +1,2 @@
-let numberOne = 10;
-let numberTwo = 20;
-let stringOne = "10";
-let stringTwo = "20";
-
-// Ouput
-console.log(numberOne + "" + numberTwo); // Normal Concatenate => 1020
-console.log(stringOne + stringTwo); // Normal Concatenate => String
-console.log(`${numberOne}${numberTwo}`); // Template Literals Way => 1020
-console.log(`${stringOne}${stringTwo}`); // Template Literals Way => String
-
-console.log(numberOne + "\n" + numberTwo);
-
-console.log(`${numberOne}
-${numberTwo}`);
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
