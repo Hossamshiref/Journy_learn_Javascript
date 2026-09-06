18e192731dd29c93c843391281110a381a0ef8c9
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-06 16:38:23 +0300

    Assignment_1

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-let a = 10;
-let b = "20";
-let c = 80;
+let num = 3;
 
-console.log(+b++); //20
-console.log(++c); //81
-console.log(a--); //10
-console.log(a); //9
-console.log(++a + +b++ + +c++ - +a++); //102
-console.log(++a + -b + +c++ - -a++ + +a); //97
-console.log(--c + +b + --a * a + --a - +true); //258
+// Solution One
+console.log(num * --num); // 6
+
+// Solution Two
+console.log(num * ++num); // 6
+
+// Soultion Three
+console.log(num * num - num); // 6
+
+// Soultion Four
+console.log(++num + ++num - --num + --num + --num - num - num); // 6
+
+// Solution Five
+console.log((num += num * 2)); // 6
+
+// Solution Six
+console.log(num); // 6
