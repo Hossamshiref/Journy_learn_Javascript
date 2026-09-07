efdee58f545f0a4a281b43384fe452dad2063c69
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-07 21:55:38 +0300

    Assignment_2

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-let num = 3;
+let num = "10";
 
 // Solution One
-console.log(num * --num); // 6
+console.log(+num + +num); // 20
 
 // Solution Two
-console.log(num * ++num); // 6
+console.log((num = +num + +num)); // 20
 
-// Soultion Three
-console.log(num * num - num); // 6
+// Solution Three
+console.log((num += num % num)); // 20
 
-// Soultion Four
-console.log(++num + ++num - --num + --num + --num - num - num); // 6
-
-// Solution Five
-console.log((num += num * 2)); // 6
-
-// Solution Six
-console.log(num); // 6
+// Solution Four
+console.log(num); // 20
