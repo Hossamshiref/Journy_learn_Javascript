32da024b784d4ab708158c93592a67e9cdb425b2
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-09 00:14:14 +0300

    String Methods Part 3

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 let name = "Hossam shiref";
 
-console.log(name.indexOf("s"));
-console.log(name.indexOf("s", 4));
-console.log(name.indexOf("z"));
+console.log(name.length);
 
-console.log(name.lastIndexOf("s"));
-console.log(name.lastIndexOf("z"));
+console.log(name.substring(7));
+console.log(name.substring(7, 12));
+console.log(name.substring(12, 7));
+console.log(name.substring(-7, 6));
+console.log(name.substring(name.length - 13, name.length - 7));
 
-console.log(name.slice(5, 11));
-console.log(name.slice(-6));
-console.log(name.slice(-6, -2));
-console.log(name.slice(2, -2));
+console.log(name.substr(0));
+console.log(name.substr(0, 6));
+console.log(name.substr(7, 5));
+console.log(name.substr(-6, 6));
 
-console.log(name.repeat(3));
+console.log(name.includes("o"));
+console.log(name.includes("sh", 8));
 
-console.log(name.split());
-console.log(name.split(" "));
-console.log(name.split(""));
-console.log(name.split("", 6));
+console.log(name.startsWith("H"));
+console.log(name.startsWith("h"));
+console.log(name.startsWith("s", 7));
+
+console.log(name.endsWith("f"));
+console.log(name.endsWith("m", 6));
+console.log(name.endsWith("o"));
