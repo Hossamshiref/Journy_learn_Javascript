6104d85a74e0561f1aafa2075065716a64b8b3d5
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-09 22:51:31 +0300

    Assignment_2

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-// Examples
-console.log(100_000); // 100000
-console.log(100000); // 100000
-console.log(5e4 + 5e4); // 100000
-
-// Your Solutions
-console.log(1e5); // 100000
-console.log(10e4); // 100000
-console.log(100e3); // 100000
-console.log(1000e2); // 100000
-console.log(10000e1); // 100000
-console.log(10_0000); // 100000
-console.log(100_000); // 100000
-console.log(1_00000); // 100000
-console.log(5e4 + 5e4); // 100000
-console.log(1000_00); // 100000
+console.log(Number.MIN_SAFE_INTEGER * -1); // 9007199254740991
