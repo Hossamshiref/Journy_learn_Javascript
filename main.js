41ee3138430eebb9f373672a3137a5254229f721
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-10 20:02:42 +0300

    Comparison Operators

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-let word = "Elzero";
-let letterZ = "z";
-let letterE = "e";
-let letterO = "O";
+console.log(10 == 10); //true
+console.log(10 == "10"); //true
+console.log(10 != "10"); //false
 
-console.log(word.includes(letterZ)); // True
-console.log(word.includes(letterE.toUpperCase())); // True
-console.log(word.endsWith(letterO.toLowerCase())); // True
+console.log(10 === "10"); //false
+console.log(10 === 10); //true
+console.log("10" === "10"); //true
+console.log("10" !== "10"); //false
+console.log("Hossam" === "Osama"); //false
+console.log(typeof "Hossam" === typeof "Osama"); //true
+
+console.log(10 > 10); //false
+console.log(10 > 5); //true
+console.log(10 >= 10); //true
+console.log(10 < 10); //false
+console.log(10 < 20); //true
+console.log(10 <= 10); //true
+console.log("10" <= 10); //true
