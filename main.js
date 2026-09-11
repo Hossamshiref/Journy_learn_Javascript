32bed42e5f6d8c0e608caabd4134fb98d5f514c9
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-11 21:35:39 +0300

    Logical Operators

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-console.log(10 == 10); //true
-console.log(10 == "10"); //true
-console.log(10 != "10"); //false
+console.log(!true);
+console.log(!(10 == "10"));
+console.log(!(10 === "10"));
 
-console.log(10 === "10"); //false
-console.log(10 === 10); //true
-console.log("10" === "10"); //true
-console.log("10" !== "10"); //false
-console.log("Hossam" === "Osama"); //false
-console.log(typeof "Hossam" === typeof "Osama"); //true
+console.log(10 > 5 && 10 == "10" && typeof "hossam" == typeof "ahmed");
+console.log(10 > 5 && 10 == "10" && "hossam" == "ahmed");
 
-console.log(10 > 10); //false
-console.log(10 > 5); //true
-console.log(10 >= 10); //true
-console.log(10 < 10); //false
-console.log(10 < 20); //true
-console.log(10 <= 10); //true
-console.log("10" <= 10); //true
+console.log(10 > 5 || 10 === "10" || "hossam" == "ahmed");
+console.log(10 < 5 || 10 === "10" || "hossam" == "ahmed");
+console.log(10 > 5 || 10 == "10" || "hossam" == "ahmed");
