761eef7178e540617f2827fb4752c2df76e2e637
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-13 14:38:25 +0300

    Nullish Coalescing Operator And Logical Or

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
-let name = "Hossam";
-let gender = "Male";
-let theAge = 15;
+let num;
+let price = 0;
 
-gender === "Male" ? console.log("Mr") : console.log("Mrs");
-
-console.log("==============================");
-
-let result = gender === "Male" ? "Mr" : "Mrs";
-
-document.write(result);
-
-// gender === "Male"
-//   ? console.log(`Hello Mr ${name}
-// your theAge is:${thetheAge}`)
-//   : console.log(`Hello Mrs ${name}
-// your theAge is:${thetheAge}`);
-
-console.log(`Hello ${gender === "Male" ? "Mr" : "Mrs"} ${name}
-your theAge is: ${theAge}`);
-
-console.log(
-  theAge > 20
-    ? 20
-    : theAge < 60 && theAge > 20
-      ? "20 to 60"
-      : theAge > 60
-        ? "bigger 60"
-        : "unknown",
-);
+console.log("The price is", price || 200);
+console.log("The price is", price ?? 200);
+console.log("The price is", num ?? 200);
