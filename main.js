c9d8748a4f020dee9317c5033306ce625caf8b83
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-14 21:42:43 +0300

    Assignment_3

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-let num1 = 10;
-let num2 = 20;
+let a = 20;
+let b = 30;
+let c = 10;
 
-console.log(num1 <= num2); // true
-console.log(num1 < num2); // true
-console.log(num2 > num1); // true
-console.log(num1 != num2); // true
-console.log(num1 !== num2); // true
-console.log(typeof num1 == typeof num2); // true
+console.log((a < b && a > c) || a == b); // true
+console.log(a < b > a != c); // true
+console.log(!(a > b) && !(a > b) && !(a < c) && !(a < c)); // true
