cc2ab50dcfca8affd001c1f6e63b743d299d49ed
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-16 15:17:09 +0300

    Assignment_6

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-let num1 = 9;
-let str = "9";
-let str2 = "20";
+let num1 = 10;
+let num2 = 30;
+let num3 = "30";
 
-if (num1 === str) {
-  console.log("{num1} Is The Same Value As {str}");
-} else if (num1 == str && typeof num1 !== typeof str) {
-  console.log("{num1} Is The Same Value As {str} But Not The Same Type");
-} else if (num1 !== str2 && typeof num1 !== typeof str2) {
-  console.log("{num1} Is Not The Same Value Or The Same Type As {str2}");
-} else if (str !== str2 && typeof str === typeof str2) {
-  console.log("{str} Is The Same Type As {str2} But Not The Same Value");
+if (num3 > num1 && typeof num3 !== typeof num2) {
+  console.log(
+    "30 Is Larger Than 10 And Type string Not The Same Type As number",
+  );
+} else if (num3 > num1 && typeof num3 !== typeof num2 && num3 == num2) {
+  console.log(
+    "30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number",
+  );
+} else if (
+  num3 !== num1 &&
+  typeof num3 !== typeof num1 &&
+  typeof num3 !== typeof num2
+) {
+  console.log(
+    "{num3} Value And Type Is Not The Same As {num1} And Type Is Not The Same As {num2}",
+  );
 }
-// Output
-// "{num1} Is The Same Value As {str}"
-// "{num1} Is The Same Value As {str} But Not The Same Type"
-// "{num1} Is Not The Same Value Or The Same Type As {str2}"
-// "{str} Is The Same Type As {str2} But Not The Same Value"
+
+// Needed Output
+// "30 Is Larger Than 10 And Type string Not The Same Type As number"
+// "30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"
+// "{num3} Value And Type Is Not The Same As {num1} And Type Is Not The Same As {num2}"
