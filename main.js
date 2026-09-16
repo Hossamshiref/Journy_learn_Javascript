d325b2ff2ab199056cd4b60a94da41251f2caefb
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-16 15:09:04 +0300

    Assignment_5

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-// Test Case 1
-// let num = 9; // "009"
+let num1 = 9;
+let str = "9";
+let str2 = "20";
 
-// Test Case 2
-// let num = 20; // "020"
-
-// Test Case 3
-let num = 110; // "110"
-
-if (num < 10) {
-  console.log(`00${num}`);
-} else if (num > 10 && num < 100) {
-  console.log(`0${num}`);
-} else if (num >= 100) {
-  console.log(num);
+if (num1 === str) {
+  console.log("{num1} Is The Same Value As {str}");
+} else if (num1 == str && typeof num1 !== typeof str) {
+  console.log("{num1} Is The Same Value As {str} But Not The Same Type");
+} else if (num1 !== str2 && typeof num1 !== typeof str2) {
+  console.log("{num1} Is Not The Same Value Or The Same Type As {str2}");
+} else if (str !== str2 && typeof str === typeof str2) {
+  console.log("{str} Is The Same Type As {str2} But Not The Same Value");
 }
+// Output
+// "{num1} Is The Same Value As {str}"
+// "{num1} Is The Same Value As {str} But Not The Same Type"
+// "{num1} Is Not The Same Value Or The Same Type As {str2}"
+// "{str} Is The Same Type As {str2} But Not The Same Value"
