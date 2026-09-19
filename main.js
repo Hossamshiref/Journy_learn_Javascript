b6c1a0bb27f17e9387068010ee9af06958c77e73
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-19 22:11:22 +0300

    Sorting Arrays

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-let names = ["Ahmed", "Hossam", "Shiref", "Ahmed"];
+let names = ["Ahmed", "Hossam", 1, "10", 90, "1000", 50];
 
+console.log(names.sort());
+console.log(names.reverse());
+console.log(names.sort().reverse());
 console.log(names);
-
-console.log(names.indexOf("Ahmed"));
-console.log(names.indexOf("Mariam"));
-console.log(names.indexOf("Ahmed", 2));
-
-console.log(names.lastIndexOf("Ahmed"));
-console.log(names.lastIndexOf("Mariam"));
-console.log(names.lastIndexOf("Ahmed", 2));
-
-console.log(names.includes("Ahmed"));
-console.log(names.includes("Mariam"));
