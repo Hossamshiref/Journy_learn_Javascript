4b80ac02d0e22d773752c3e8486cf894bf335dad
Hossam Shiref <hossamshiref01@gmail.com>
2026-09-20 16:07:38 +0300

    Slicing Array

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-let names = ["Ahmed", "Hossam", 1, "10", 90, "1000", 50];
+let names = ["Ahmed", "Hossam", "Mariam", "Esraa"];
 
-console.log(names.sort());
-console.log(names.reverse());
-console.log(names.sort().reverse());
+console.log(names.slice());
+console.log(names.slice(0, 3));
+console.log(names.slice(1, 2));
+
+console.log(names);
+
+names.splice();
+names.splice(2, 1, "Shiref", "Ganna");
+
 console.log(names);
