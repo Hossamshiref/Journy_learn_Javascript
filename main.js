e104afb67c6347131938f66953fd5f93312b174b
hossamshiref01@gmail.com <hossamshiref01@gmail.com>
2026-09-24 18:53:32 +0300

    Looping On Sequences

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-//for loop
-
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+let names = [1, 2, 3, "Hossam", "Shiref", "Ahmed", "Mohamed", "Osama"];
+let only = [];
+for (let i = 0; i < names.length; i++) {
+  if (typeof names[i] === "string") {
+    only.push(names.at(i));
+  }
 }
+console.log(only);
 
-console.log("============================================");
+console.log("=============================");
 
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// console.log(names[4]);
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
 }
